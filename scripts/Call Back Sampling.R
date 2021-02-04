51513113b7cdb46414849a243255ea3e246aff75
Tumaini Kilimba <datakilimba@gmail.com>
2021-02-04 11:27:47 +0300

    Deleted unnecessary calls to postgres database

--- scripts/Call Back Sampling.R
+++ scripts/Call Back Sampling.R
@@ -1,10 +1,25 @@
 library(tidyverse)
 library(xlsx)
 library(magrittr)
-library(RPostgres)
 
-con = DBI::dbConnect(odbc::odbc(),"PostgreSAKiRP")
-waeo = dbReadTable(con,"waeos") %>% 
-  filter(active == 1)
-district = dbReadTable(con, "districts")
-ward = dbReadTable(con, "wards")
+hhs_general_survey = readxl::read_xlsx(paste0("C:/Users/tnkil/OneDrive/Enabel",
+" - SAKiRP/M&E/Data Collection/S01 - HH Survey/Data Analysis/Calculation Sheets ",
+"2020/Houshold Survey 2020 Sample Sets.xlsx"),
+sheet = "HHS general sample")
+
+hhs_general_survey_beneficiaries_male = hhs_general_survey %>%
+  filter(`Is the respondent a member of a group or not?` == "Member of a group",
+         `Sex of respondent` == "Male",
+         `Phone number of HHH or other member of the HH.`!=999) %>% 
+  mutate(`Status (X/V)` = "",`# B` = "") %>% 
+  rename(Name = `Select beneficiary name`,
+         `Phone 1` = `Phone number of HHH or other member of the HH.`,
+         `Phone 2` = `2nd phone number of HHH or other member of the HH.`,
+         Sex = `Sex of respondent`) %>% 
+  mutate(`Phone 2` = na_if(`Phone 2`,999)) %>% 
+  select(Name,`Phone 1`,`Phone 2`,Sex ,District, Ward, Village,WAEO,
+         `Status (X/V)`,`# B`)
+
+
+
+
